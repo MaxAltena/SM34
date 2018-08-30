4cf5de8fda6820201a6433f4af6393927dfbcd27
MaxAltena <Max@MaxAltena.com>
2018-08-30 14:44:04 +0200

    next commit

--- 1-HelloWorld/HelloWorld.playground/Contents.swift
+++ 1-HelloWorld/HelloWorld.playground/Contents.swift
@@ -5,12 +5,16 @@ import PlaygroundSupport
 
 class MyViewController : UIViewController {
     override func loadView() {
+        let numberOfFacesOnDice = 6
+        let pi = 3.1415
+        let justSomeText = "Hello there!"
+        
         let view = UIView()
         view.backgroundColor = .white
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
+        label.text = "Doei deze wereld!"
         label.textColor = .black
         
         view.addSubview(label)
