b8231f56aa328801737cbbc868841aad6b83f1ec
MaxAltena <Max@MaxAltena.com>
2018-09-07 17:14:34 +0200

    Completed Ratings-2

---
+++ Week2/Ratings-2/Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by Max Altena on 07/09/2018.
+//  Copyright © 2018 Max Altena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
